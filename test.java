a19b2c31e7d01aae48dd1c0a36c72d7e8277e85e
elhoussam <houssam.glsd@gmail.com>
2017-07-05 13:11:32 +0100

    Try Ssh connection

---
+++ test.java
@@ -0,0 +1,13 @@
+bin
+README.md
+res
+Snake2.jar
+src
+test.java
+total 76
+drwxrwxr-x 3 el-houssam el-houssam  4096 يول  5 12:51 bin
+-rw-rw-r-- 1 el-houssam el-houssam   195 يول  5 12:05 README.md
+drwxrwxr-x 3 el-houssam el-houssam  4096 يول  5 01:00 res
+-rw-rw-r-- 1 el-houssam el-houssam 53505 يول  5 11:24 Snake2.jar
+drwxrwxr-x 2 el-houssam el-houssam  4096 يول  5 01:33 src
+-rw-rw-r-- 1 el-houssam el-houssam    43 يول  5 13:10 test.java
